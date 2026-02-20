9666c7fa412d5c18b4d24f6c330b5033da714e2e
somethingGit <jtse1130@gmail.com>
2026-02-20 15:40:27 -0700

    Added breakpoint

--- Dietary_Info/src/routes/login/+page.svelte
+++ Dietary_Info/src/routes/login/+page.svelte
@@ -9,7 +9,7 @@
         <div class="ml-0 mt-0 w-full h-1/15 bg-secondary-dark">
             <div class="bg-secondary-dark w-1/4 pl-5 pt-3 pb-3">
                 <h1 class="text-primary-bright text-3xl">
-                    <b>PROJECT OCTALNAUTS</b>
+                    <b>PROJECT<br>OCTALNAUTS</b>
                 </h1>
             </div>
         </div>
