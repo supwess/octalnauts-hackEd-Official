b7b53351993201e097c8bcaa3c22ed1d18d95b96
Wesley Supangat <44500655+supwess@users.noreply.github.com>
2026-02-20 14:19:26 -0700

    fix pls

---
+++ Dietary_Info/src/routes/login/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import "../../app.css"
+</script>
+
+<body class="bg-primary-dark">
+    <div class="flex justify-center align-middle items-center mx-auto place-items-center h-screen">
+        <form class="">
+            <div class="text-primary-bright">
+                <label>Email: </label>
+                <input type="email" required class="bg-secondary-dark rounded-lg border-solid outline-secondary-light outline-3 p-3 w-full overflow-hidden">
+
+                <br><br>
+
+                <label>Password: </label>
+                <input type="password" required>
+            </div>
+            <br>
+            <div>
+                <button class="text-primary-bright justify-center">Login</button>
+            </div>
+        </form>
+    </div>
+    
+</body>
+
