345fd5c3c1ec1d31554e210e02fed034a3ee90a7
Kalyn Luc <kalynluc@gmail.com>
2026-02-20 12:48:59 -0700

    page added buttons

--- Dietary_Info/src/routes/home/+page.svelte
+++ Dietary_Info/src/routes/home/+page.svelte
@@ -1,1 +1,17 @@
-<h1>Welcome to Dietary Info</h1>
+<h1>
+    Dietary Info
+</h1>
+
+<script>
+    function goLoginPage() {
+        window.location.href = '/login';
+    }
+</script>
+
+<button on:click={goLoginPage}>
+    Login
+</button>
+
+<button>
+    Continue as Guest
+</button>
