af3cb67662e5726e4f0c4ac0e30d5c3b3eaaf58d
kalyn <kalynluc@gmail.com>
2026-02-20 14:52:12 -0700

    added login and guest images

--- Dietary_Info/src/routes/home/+page.svelte
+++ Dietary_Info/src/routes/home/+page.svelte
@@ -3,15 +3,70 @@
 </h1>
 
 <script>
-    function goLoginPage() {
+    function goLoginPage(){
         window.location.href = '/login';
+    }
+    function goGuest(){
+        alert("Continuing as Guest....");
     }
 </script>
 
-<button on:click={goLoginPage}>
-    Login
-</button>
+<div class="buttons">
+    <button class="loginButton"
+        on:click={goLoginPage}>
+    </button>
+    <button class="guestButton"
+        on:click={goGuest}>
+    </button>
+</div>
 
-<button>
-    Continue as Guest
-</button>
+<style>
+    :global(body){
+        background-color: lightblue;
+        margin: 0;
+        font-family: sans-serif;
+       /* cursor: url('/images/apple.jpg'), auto; */
+    }
+    h1{
+        text-align: center;
+        font-size: 6rem;
+        margin-top: 7rem;
+    }
+    .buttons{
+        display: flex;
+        justify-content: center;
+        gap: 2rem;
+        margin-top: 0rem;
+    }
+    .loginButton{
+        width: 10rem;
+        height: 10rem;
+        background-image: url('/images/login.jpg');
+        background-size: cover;       /*cover whole button*/
+        background-position: center;  /*center image*/
+        border-radius: 8px;           /*rounded corners*/
+        cursor: url('/images/login2.jpg'), auto;
+    }
+    .loginButton:hover{
+        background-image: url('/images/login2.jpg');
+    }
+    .guestButton{
+        width: 10rem;
+        height: 10rem;
+        background-image: url('/images/guest.jpg');
+        background-size: cover;       /*cover whole button*/
+        background-position: center;  /*center image*/
+        border-radius: 8px;           /*rounded corners*/
+    }
+    .guestButton:hover{
+        background-image: url('/images/guest2.jpg');
+    }
+    button{
+        width: 10rem;
+        height: 10rem;
+        cursor: pointer;    /*cursor change on buttons*/
+    }
+    button:hover{
+        background-color: #27a478;
+    }
+</style>
