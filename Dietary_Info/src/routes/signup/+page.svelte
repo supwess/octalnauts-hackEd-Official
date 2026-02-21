598157e29f0f3246c440f46735caab3ebac5a3e1
Wesley Supangat <44500655+supwess@users.noreply.github.com>
2026-02-20 17:44:01 -0700

    Added sign up page

--- Dietary_Info/src/routes/signup/+page.svelte
+++ Dietary_Info/src/routes/signup/+page.svelte
@@ -25,14 +25,22 @@
     <div class="flex justify-center items-center h-svh">
         <form class="w-2/3 text-primary-bright">
             <div>
-                <h1 class="text-center text-4xl"><b>Log In</b></h1>
+                <h1 class="text-center text-4xl"><b>Sign Up</b></h1>
             </div>
 
             <br><br>
 
             <div>
-                <label><b>Username or Email</b></label><br>
-                <input type="text" required placeholder="Enter Username or Email"
+                <label><b>Username</b></label><br>
+                <input type="text" required placeholder="Enter Username"
+                    class="w-full bg-secondary-dark rounded-lg outline-2 outline-secondary-light p-3 mt-2">
+            </div>
+
+            <br>
+
+            <div>
+                <label><b>Email</b></label><br>
+                <input type="email" required placeholder="Enter Email"
                     class="w-full bg-secondary-dark rounded-lg outline-2 outline-secondary-light p-3 mt-2 invalid:text-pink-600 invalid:outline-pink-500">
             </div>
 
@@ -48,14 +56,14 @@
 
             <div class="justify-end flex">
                 <button class="w-2/5 text-primary-bright bg-secondary-dark rounded-xl p-2 hover:bg-secondary-light hover:cursor-pointer transition-all duration-150 mt-3" >
-                    Login
+                    Sign up
                 </button>
             </div>
 
             <br>
 
             <div>
-                <p class="text-center">Don't have an account? <a href="../signup" class="text-blue-500 hover:text-blue-600 transition-all duration-150">Create an account</a></p>
+                <p class="text-center">Already have an account? <a href="../login" class="text-blue-500 hover:text-blue-600 transition-all duration-150">Log in</a></p>
             </div>
         </form>
     </div>
